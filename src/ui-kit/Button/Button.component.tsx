7aff8252971a687770174817aa03bbe557baa023
Eugene Malchenko <nijoelite@gmail.com>
2019-11-11 06:21:33 +0300

    Update button transition

--- src/ui-kit/Button/Button.component.tsx
+++ src/ui-kit/Button/Button.component.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled/macro';
 
 export const Button = styled('button')<{small?: boolean}>`
-  background: none;
+  background: #ffc608;
   border: 1px solid #ffc608;
   border-radius: 5px;
   padding: 10px 20px;
   cursor: pointer;
-  transition: 0.1s linear;
+  transition: 0.12s linear;
   display: inline-block;
   text-align: center;
   color: #000000;
@@ -14,8 +14,7 @@ export const Button = styled('button')<{small?: boolean}>`
   ${({small}): string => small ? 'padding: 5px 10px;' : ''}
 
   &:hover {
-    background: #ffc608;
-    color: #000000;
+    box-shadow: 0px 0px 0 1px #ffc608;
     text-decoration: none;
   }
 `;
