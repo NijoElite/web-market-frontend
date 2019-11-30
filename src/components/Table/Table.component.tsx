7dd8d614628a5a43157fd2e7f0d4f980d579f2ef
Eugene Malchenko <nijoelite@gmail.com>
2019-11-30 04:35:34 +0300

    Add Table component

---
+++ src/components/Table/Table.component.tsx
@@ -0,0 +1,99 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled/macro';
+import { transition } from '../../utils/css.utils';
+
+// #region styled
+const TableRowStyled = styled.div`
+  width: 100%;
+  display: flex;
+  background: #fff;
+  padding: 15px 25px;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  background: transparent;
+  border-radius: 15px;
+  ${transition('all')}
+
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
+`;
+
+const TableCellStyled = styled('div')<{ description?: string }>`
+  flex-basis: 0;
+  flex-grow: 1;
+  font-size: 16px;
+
+  &:after {
+    content: '${({ description }): string => description || ''}';
+    display: block;
+    font-size: 12px;
+  }
+`;
+
+const TableStyled = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+
+  > ${TableRowStyled}:nth-child(2n) {
+    background: #f9fafe;
+  }
+  > ${TableRowStyled}:nth-child(2n - 1) {
+    background: #fff;
+  }
+`;
+
+const DropTableStyled = styled('div')`
+  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 15px 0;
+  width: 100%;
+
+  > ${TableRowStyled}:hover {
+    background: #ffc608;
+  }
+`;
+// #endregion
+
+interface NestedTableProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const Table: FC<NestedTableProps> = ({ className, children }) => {
+  return <TableStyled className={className}>{children}</TableStyled>;
+};
+
+interface TabelCellProps {
+  className?: string;
+  description?: string;
+  children?: React.ReactNode;
+}
+
+export const TableCell: FC<TabelCellProps> = ({ children, description, className }) => {
+  return (
+    <TableCellStyled description={description} className={className}>
+      {children}
+    </TableCellStyled>
+  );
+};
+
+interface TabelRowProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const TableRow: FC<TabelRowProps> = ({ children, className }) => {
+  return <TableRowStyled className={className}>{children}</TableRowStyled>;
+};
+
+interface DropTableRowProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const DropTable: FC<DropTableRowProps> = ({ children, className }) => {
+  return <DropTableStyled className={className}>{children}</DropTableStyled>;
+};
