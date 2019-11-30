72d92e8d85d80d65c8ba88974b52b5c047b1f8a2
Eugene Malchenko <nijoelite@gmail.com>
2019-11-30 05:14:23 +0300

    Add roles panel to cabinet

--- src/pages/CabinetPage/CabinetLayot.component.tsx
+++ src/pages/CabinetPage/CabinetLayot.component.tsx
@@ -1,8 +1,82 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Container } from '../../ui-kit/Container/Container.component';
+import { UserApi } from '../../services/User/UserApi';
+import { connect } from 'react-redux';
+import { AppState } from '../../store';
+import styled from '@emotion/styled/macro';
+import { Link } from 'react-router-dom';
 
-const CabinetLayout: FC = ({ children }) => {
-  return <Container>{children}</Container>;
+// #region styled
+const RolesPanel = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+
+const RoleButton = styled(Link)`
+  padding: 8px 20px;
+  background: #fff;
+  border-radius: 20px;
+  margin-right: 15px;
+  text-decoration: none;
+  color: #000;
+  transition: all 0.1s linear;
+
+  &:hover {
+    background: #ffc608;
+  }
+`;
+// #endregion
+
+const links: { [key: string]: [string, string] } = {
+  user: ['Пользователь', '/cabinet'],
+  admin: ['Администратор', '/cabinet/admin'],
+  customer: ['Продавец', '/cabinet/customer'],
 };
 
-export default CabinetLayout;
+interface StateProps {
+  userId?: string;
+}
+
+type Props = StateProps;
+
+const CabinetLayout: FC<Props> = ({ children, userId }) => {
+  const [userRoles, setUserRoles] = useState<string[]>([]);
+  const [isFetched, setFetched] = useState(false);
+
+  useEffect(() => {
+    const fetchUserData = async (): Promise<void> => {
+      const response = await UserApi.getUser(userId || '');
+      setFetched(true);
+
+      if (response.status === 'success') {
+        setUserRoles(response.data.user.role);
+      }
+    };
+
+    if (!isFetched) {
+      fetchUserData();
+    }
+  });
+
+  return (
+    <Container>
+      <RolesPanel>
+        {userRoles.map(role => (
+          <RoleButton to={links[role][1]}>{links[role][0]}</RoleButton>
+        ))}
+      </RolesPanel>
+      {children}
+    </Container>
+  );
+};
+
+// #region Map to Props
+const mapStateToProps = (root: AppState): StateProps => {
+  return {
+    userId: root.system.userId,
+  };
+};
+
+// #endregion
+
+export default connect(mapStateToProps)(CabinetLayout);
